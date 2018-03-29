7ba052881c573e3cff481d139a6de99e6df91cee
bthipavo <bthipavo@gmail.com>
2018-03-28 23:20:20 -0700

    started web page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import * as mat from 'react-materialize'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <mat.Navbar brand="Arcade Warrior" right>
+          <mat.NavItem href='#'>Getting started</mat.NavItem>
+          <mat.NavItem href='#'>Components</mat.NavItem>
+        </mat.Navbar>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
