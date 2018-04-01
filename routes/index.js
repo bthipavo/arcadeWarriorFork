e552a050622bfa56b0cafbb53338f847eee0773a
bthipavo <bthipavo@gmail.com>
2018-03-31 20:06:23 -0700

    added login and UI/UX

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+// Get Homepage
+router.get('/', ensureAuthenticated, function(req, res){
+	res.render('index');
+});
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		res.redirect('/users/login');
+	}
+}
+
+module.exports = router;
