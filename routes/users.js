b10dd5eb5afe90cfabd94e10e6c1383896bedca7
bthipavo <bthipavo@gmail.com>
2018-04-07 00:41:43 -0700

    fixed register page

--- routes/users.js
+++ routes/users.js
@@ -23,6 +23,10 @@ router.get('/mario', function(req, res){
 	res.render('mario');
 });
 
+router.get('/galaga', function(req, res){
+	res.render('galaga');
+});
+
 // Register User
 router.post('/register', function(req, res){
 	var name = req.body.name;
